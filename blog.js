c40504bae0772a12c305bcfb875d08b530b103c0
Parvinkhuki <khuki.developer@gmail.com>
2023-09-01 04:26:24 +0600

    done

--- blog.js
+++ blog.js
@@ -3,3 +3,9 @@ const btn=document.getElementById("blog")
 const blog=()=>{
     location.href="blog.html";
         };
+
+        const bttn=document.getElementById("back")
+
+const back=()=>{
+    location.href="index.html";
+        };
